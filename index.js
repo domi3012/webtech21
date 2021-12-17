33dc33a4f2066b958a8a547b06cfc2aff6a21031
Dominik König <d.koenig@student.tugraz.at>
2021-12-17 14:50:38 +0100

    voting system implementation. needs refactoring, but is working, and dont think we get points for the cs(it should work)

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ var users = []
 
 var currentUsers = []
 
+var activeSession = false; //TODO change back to false
+
 var urlencodeParser = bodyParser.urlencoded({extended: false})
 
 const oneDay = 1000 * 60 * 60 * 24;
 
-var votingDictionary = {}
+var votingQuestionsDictionary = {}
+var votingAnswersDictionary = {}
 
 //session middleware
 app.use(sessions({
@@ -55,6 +58,7 @@ app.post('/loginUser', urlencodeParser, (req, res) => {
         if (k.password === req.body.password && k.username === req.body.username) {
             session = req.session;
             session.userid = req.body.username;
+            activeSession = true
             res.redirect("/index.html")
             found = true;
             return
@@ -67,7 +71,6 @@ app.post('/loginUser', urlencodeParser, (req, res) => {
 })
 
 app.get('/register.html', function (req, res) {
-    activeSession = false
     if (session && session.userid) {
         activeSession = true;
     }
@@ -76,6 +79,7 @@ app.get('/register.html', function (req, res) {
 
 app.get('/logout.html', function (req, res) {
     session = undefined;
+    activeSession = false
     res.redirect("index.html");
 });
 
@@ -87,6 +91,11 @@ app.get('/login.html', function (req, res) {
     res.render("login", {error: false, loggedIn: activeSession});
 });
 
+
+app.get('/', function (req, res){
+    res.redirect("index.html")
+})
+
 app.get('/index.html', function (req, res) {
     let keys = Object.keys(questions);
     let indexData = [];
@@ -94,15 +103,15 @@ app.get('/index.html', function (req, res) {
         questions[keys[i]].key = keys[i]
         indexData.push(questions[keys[i]])
         let index = parseInt(keys[i])
-        if (!(index in votingDictionary)){
-            votingDictionary[index] = {"upVotes": 0, "downVotes": 0}
+        if (!(index in votingQuestionsDictionary)){
+            votingQuestionsDictionary[index] = {"upVotes": 0, "downVotes": 0}
         }
     }
     if (session && session.userid) {
-        res.render("index", {loggedIn: true, text: indexData, voting: votingDictionary});
+        res.render("index", {loggedIn: true, text: indexData, voting: votingQuestionsDictionary});
         return
     }
-    res.render("index", {loggedIn: false, text: indexData, voting: votingDictionary});
+    res.render("index", {loggedIn: false, text: indexData, voting: votingQuestionsDictionary});
 });
 
 app.get("/search", urlencodeParser, (req, res) => {
@@ -115,13 +124,21 @@ app.get("/question/:qid", urlencodeParser, (req, res) => {
     let keys = Object.keys(answers);
     let answer = [];
     keys.forEach((e) => {
+        if (!(e in votingAnswersDictionary)){
+            votingAnswersDictionary[e] = {"upVotes": 0, "downVotes": 0}
+        }
         if (answers[e].ParentId === parseInt(req.params.qid)){
             answers[e].key = e
             console.log(answers[e]["key"])
             answer.push(answers[e]);
         }
     })
-    res.render("question", {question: question, answers: answer, loggedIn: false}) //TODO change to active session
+    if (!Object.keys(votingAnswersDictionary).length || !Object.keys(votingQuestionsDictionary).length) {
+        console.log("test");
+        res.redirect("../index.html")
+        return
+    }
+    res.render("question", {question: question, answers: answer,ansVoting: votingAnswersDictionary, questVoting: votingQuestionsDictionary, loggedIn: activeSession}) //TODO change to active session
 })
 
 app.post("/vote", urlencodeParser, (req, res) => {
@@ -131,10 +148,33 @@ app.post("/vote", urlencodeParser, (req, res) => {
         return
     }
     if (req.body.upvote !== undefined &&req.body.upvote.toString().includes("Upvote")) {
-        votingDictionary[key].upVotes += 1
+        votingQuestionsDictionary[key].upVotes += 1
     } else
-        votingDictionary[key].downVotes += 1;
+        votingQuestionsDictionary[key].downVotes += 1;
     res.redirect("/index.html")
+    return
+})
+
+app.post("/Ansvote", urlencodeParser, (req, res) => {
+    let key = parseInt(req.body.key)
+    let parentKey = parseInt(req.body.parentkey)
+    if (!activeSession){
+        let redirectQuestion = ((!isNaN(parentKey)) ? parentKey : key);
+        res.redirect("/question/"+redirectQuestion)
+        return
+    }
+    if (req.body.upvoteAns !== undefined &&req.body.upvoteAns.toString().includes("Upvote")) {
+        votingAnswersDictionary[key].upVotes += 1
+    }else if (req.body.upvote !== undefined &&req.body.upvote.toString().includes("Upvote")) {
+        votingQuestionsDictionary[key].upVotes += 1
+    } else if(req.body.downvote !== undefined &&req.body.downvote.toString().includes("Downvote")){
+        votingQuestionsDictionary[key].downVotes += 1;
+    }
+    else  {
+        votingAnswersDictionary[key].downVotes += 1
+    }
+    let redirectQuestion = ((!isNaN(parentKey)) ? parentKey : key);
+    res.redirect("/question/"+redirectQuestion)
     return
 
 
@@ -170,6 +210,5 @@ app.listen(3000, () => {
     var questions_json = "./input_data/Questions.json"
     var answers_json = "./input_data/Answers.json"
     readData(questions_json, answers_json)
-
     console.log(`Example app listening at http://localhost:3000`);
 });
